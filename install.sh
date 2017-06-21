1edb849a6d44b16d55d799d0a1a0db0fbacf7e6d
Dean Shaff <dshaff001@gmail.com>
2017-06-21 10:25:18 +0400

    Updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
-INSTALL_FILES=".install_files.txt"
-INSTALL_LOG=".install_log.log"
+PACKAGE_NAME="pyro4tunneling"
+INSTALL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+INSTALL_FILES="${INSTALL_DIR}/install_files_${PACKAGE_NAME}.txt"
+INSTALL_LOG="${INSTALL_DIR}/install_log_${PACKAGE_NAME}.log"
+
+function checkVirutalEnv {
+    if [[ ${VIRTUAL_ENV} == "" ]]; then
+        echo "WARNING: Installing outside a Virtual Environment"
+        echo "Proceed with installation? y/n"
+        read CONTINUE
+        if [[ ${CONTINUE} == "n" ]]; then
+            exit 0
+        fi
+    else
+        echo "Installing inside a Virtual Environment: ${VIRTUAL_ENV}"
+    fi
+}
 
 if [[ $# -eq 0 ]]; then
     echo "Specify whether you're trying to install or uninstall"
@@ -12,32 +27,23 @@ else
     ARG=$1
 fi
 
-if [[ $ARG == "-i" ]]; then
-    if [[ $VIRTUAL_ENV == "" ]]; then
-        echo "WARNING: Installing outside a Virtual Environment"
-        echo "Proceed with installation? y/n"
-        read CONTINUE
-        if [[ ${CONTINUE} == "n" ]]; then
-            exit 0
-        fi
-    else
-        echo "Installing inside a Virtual Environment: ${VIRTUAL_ENV}"
-    fi
+if [[ ${ARG} == "-i" ]]; then
+    checkVirutalEnv
     echo -n "Running setup.py..."
-    python setup.py install --record $INSTALL_FILES &> $INSTALL_LOG
+    python ${INSTALL_DIR}/setup.py install --record ${INSTALL_FILES} >>${INSTALL_LOG} 2>&1
     EXITCODE=$?
-    if [[ $EXITCODE -eq 0 ]]; then
+    if [[ ${EXITCODE} -eq 0 ]]; then
         echo -e "\rRunning setup.py... Complete!"
         exit 0
     else
         echo -e "\rRunning setup.py... Failed."
         exit 1
     fi
-elif [[ $ARG == "-u" ]]; then
+elif [[ ${ARG} == "-u" ]]; then
     echo "Attempting to uninstall"
-    if [[ -e $INSTALL_FILES ]]; then
+    if [[ -e ${INSTALL_FILES} ]]; then
         echo -n "Found egg files. Deleting... "
-        xargs rm <$INSTALL_FILES
+        xargs rm <${INSTALL_FILES}
         echo -e "\rFound egg files. Deleting... Done!"
     else
         echo "Couldn't find the files to remove"
